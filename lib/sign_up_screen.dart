8ae5fc4393bc05983d8780b444c98597fead14b6
sanjanav2102 <sanjanav2102@users.noreply.github.com>
2025-05-26 00:43:26 +0530

    Updated RichText

--- lib/sign_up_screen.dart
+++ lib/sign_up_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:uidev/sign_in_screen.dart';
 import 'custom_widgets.dart';
+import 'package:flutter/gestures.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
@@ -17,71 +19,73 @@ class SignUpScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text('Create Account',
-                style: TextStyle(
-                    fontSize: 35,
-                )
+            const Text(
+              'Create Account',
+              style: TextStyle(
+                fontSize: 35,
+              ),
             ),
             const SizedBox(height: 8),
-            Row(
-              children: [
-                const Text('Enter your Name, Email and Password\n'
-                    'for sign up. '),
-                SizedBox(height:16),
-                Center(
-                  child: GestureDetector(
-                    onTap: () {
-                      // Navigate back to sign in
-                    },
-                    child: const Text(
-                        "\nAlready have account?",
-                        style: TextStyle(
-                            color: Colors.green,
-
-                        )
-                    ),
+            RichText(
+              text: TextSpan(
+                style: const TextStyle(color: Colors.black, fontSize: 16),
+                children: [
+                  const TextSpan(
+                    text: 'Enter your Name, Email and Password for sign up. ',
                   ),
-                ),
-              ],
+                  TextSpan(
+                    text: 'Already have account?',
+                    style: const TextStyle(
+                      color: Colors.green,
+                      decoration: TextDecoration.underline,
+                    ),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const SignInScreen(),
+                          ),
+                        );
+                      },
+                  ),
+                ],
+              ),
             ),
             const SizedBox(height: 24),
-            TextField(decoration: inputDecoration(
-                "Full Name")
-            ),
+            TextField(decoration: inputDecoration("Full Name")),
             const SizedBox(height: 16),
-            TextField(decoration: inputDecoration(
-                "Email Address"
-            )
-            ),
+            TextField(decoration: inputDecoration("Email Address")),
             const SizedBox(height: 16),
             TextField(
               obscureText: true,
               decoration: inputDecoration("Password").copyWith(
-                suffixIcon: Icon(Icons.visibility_off),
+                suffixIcon: const Icon(Icons.visibility_off),
               ),
             ),
-            SizedBox(height: 16),
+            const SizedBox(height: 16),
             SizedBox(
               width: double.infinity,
               height: 50,
               child: ElevatedButton(
                 onPressed: () {},
-                style:ElevatedButton.styleFrom(
-                    backgroundColor:Colors.green,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    )
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
                 ),
                 child: const Text(
-                    "SIGN UP",
-                    style:TextStyle(
-                      color: Colors.white,
-                    )),
+                  "SIGN UP",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
               ),
             ),
             const SizedBox(height: 12),
-            Center(
-              child: const Text(
+            const Center(
+              child: Text(
                 "By Signing up you agree to our Terms\n"
                     "     Conditions & Privacy Policy.",
                 style: TextStyle(fontSize: 15),
@@ -90,9 +94,17 @@ class SignUpScreen extends StatelessWidget {
             const SizedBox(height: 16),
             const Center(child: Text("Or")),
             const SizedBox(height: 16),
-            socialButton(text: "CONNECT WITH FACEBOOK", icon: Icons.facebook, color: Colors.indigo),
+            socialButton(
+              text: "CONNECT WITH FACEBOOK",
+              icon: Icons.facebook,
+              color: Colors.indigo,
+            ),
             const SizedBox(height: 8),
-            socialButton(text: "CONNECT WITH GOOGLE", icon: Icons.g_mobiledata, color: Colors.blue),
+            socialButton(
+              text: "CONNECT WITH GOOGLE",
+              icon: Icons.g_mobiledata,
+              color: Colors.blue,
+            ),
           ],
         ),
       ),
